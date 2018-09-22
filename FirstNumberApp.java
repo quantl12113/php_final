db9fec84efcb0e135caa56ffd359d2a5f5d81bc6
Nguyễn Hoàng Dũng <nhdzung00000@gmail.com>
2018-09-22 16:48:20 +0000

    FirstNumberApp.java edited online with Bitbucket

--- FirstNumberApp.java
+++ FirstNumberApp.java
@@ -1,6 +1,9 @@
 public class FirstNumberApp {
     public void checkNumber(int number){
-        NumberUtil.checkSquareNumber(number);
+        if(NumberUtil.checkSquareNumber(number))
+            System.out.println(number+" is square number");
+        else
+            System.out.println(number+" is not square number");
         NumberUtil.checkPerfectNumber(number);
     }
 }
